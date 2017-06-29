d799d8383fa00362241eebf69cdb3308597ad4c2
zmj <zach.jeyakaran@gmail.com>
2017-06-29 16:06:51 -0400

    working on parsing

--- diff.go
+++ diff.go
@@ -8,5 +8,14 @@ type deckDiff struct {
 }
 
 func diff(from, to deck) deckDiff {
+	return deckDiff{
+		from:    from,
+		to:      to,
+		Added:   nil,
+		Removed: nil,
+	}
+}
 
+func (d *deckDiff) String() string {
+	return ""
 }
--- parse.go
+++ parse.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"regexp"
+)
+
 type deck struct {
 	Name   string
+	Class  string
 	Format string
 	Cards  []cardCount
 	Code   string
@@ -17,6 +25,63 @@ type cardCount struct {
 	Count int
 }
 
-func parse() (deck, bool) {
+func (p *parser) parse(deckRaw io.Reader) (*deck, error) {
+	scanner := bufio.NewScanner(deckRaw)
+	d := &deck{}
 
+	nextLine := func(lineName string) error {
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				return fmt.Errorf("Failed to read expected %v line: %v", lineName, err.Error())
+			}
+			return fmt.Errorf("Missing expected %v line", lineName)
+		}
+		return nil
+	}
+
+	parseLine := func(lineName string, reg *regexp.Regexp, assign func([]string)) error {
+		if err := nextLine(lineName); err != nil {
+			return err
+		}
+		if reg == nil {
+			return nil // expected empty line
+		}
+		match := reg.FindStringSubmatch(scanner.Text())
+		if match == nil || len(match) < reg.NumSubexp()+1 {
+			return fmt.Errorf("Failed to parse %v line: %v", lineName, scanner.Text())
+		}
+		assign(match)
+		return nil
+	}
+
+	if err := parseLine("name", p.Name, func(match []string) {
+		d.Name = match[1]
+	}); err != nil {
+		return nil, err
+	}
+
+	if err := parseLine("class", p.Class, func(match []string) {
+		d.Class = match[1]
+	}); err != nil {
+		return nil, err
+	}
+
+	if err := parseLine("format", p.Format, func(match []string) {
+		d.Format = match[1]
+	}); err != nil {
+		return nil, err
+	}
+
+	if err := parseLine("empty", nil, nil); err != nil {
+		return nil, err
+	}
+
+}
+
+type parser struct {
+	Name      *regexp.Regexp
+	Class     *regexp.Regexp
+	Format    *regexp.Regexp
+	CardCount *regexp.Regexp
+	Code      *regexp.Regexp
 }
